6a75a36acb60f5f5985293ae893a863e289190af
mlkb <malik_427@hotmail.com>
2021-01-07 18:32:33 +0100

    port solved

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ let botchannel
 //////////////////////////////express server///////////////////////////////////////////
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/auth/twitch/callback', (req, res) => {
     console.log("called back")
